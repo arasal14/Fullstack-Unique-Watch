55d0569a512c8b805ff6337483f24838cb6da9a4
arasal14 <106369647+arasal14@users.noreply.github.com>
2024-03-14 16:52:38 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,7 +12,7 @@ dotenv.config();
 var gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY || jht3wxns6br9mp89,
   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
 });
 
